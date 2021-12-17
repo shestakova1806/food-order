d7d30313066480da3bd33159feee5dcb2ba1bcb4
Alexandra Shestakova <shestakova18@bk.ru>
2021-12-17 10:17:33 +0300

    feat: added Cart functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food" />
